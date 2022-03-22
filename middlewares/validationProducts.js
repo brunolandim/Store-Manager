05087a77d066c86d6a77db981bddd1696dc4dd99
Bruno Landim <brunolandim13@hotmail.com>
2022-03-22 13:24:23 -0300

    Ajustesgit add .

--- middlewares/validationProducts.js
+++ middlewares/validationProducts.js
@@ -1,6 +1,6 @@
 const validateName = (req, res, next) => {
     const { name } = req.body;
-    if (!name) res.status(400).json({ message: '"name" is required' });
+    if (!name) return res.status(400).json({ message: '"name" is required' });
     if (name.length < 5) {
        return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
     }
