3c9eb55b551efe01cc361321bfa2e4ed163d2453
Bruno Landim <brunolandim13@hotmail.com>
2022-03-22 14:22:40 -0300

    tentativa de ajuste

--- models/Products.js
+++ models/Products.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const getAll = async () => {
     const [result] = await connection.execute('SELECT * FROM StoreManager.products;');
-
+    
     return result;
 };
 
